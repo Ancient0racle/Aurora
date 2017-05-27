45cc982b7c56cce7f303d59e6e6a9e483bb1fe18
Zhen <guozhenhero@gmail.com>
2017-05-27 14:20:28 +0800

    add login modal and signup modal

--- js/index.js
+++ js/index.js
@@ -25,4 +25,11 @@ $(function() {
 $('.navbar-collapse ul li a').click(function() {
     $(".navbar-collapse").collapse('hide');
 });
+$(document).ready(function(){
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square-blue',
+    radioClass: 'iradio_square-blue',
+    increaseArea: '20%' // optional
+  });
+});
 
